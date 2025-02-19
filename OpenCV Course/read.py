31c186cddaceca12e1400e610fb57e757cfcf2a6
Noh Woldetinsae <nohadhanom1@gmail.com>
2025-02-19 16:54:07 -0500

    Third commit and learned how to display a video

--- OpenCV Course/read.py
+++ OpenCV Course/read.py
@@ -1,7 +1,17 @@
 import cv2 as cv
 
-img = cv.imread("Photos/cat.jpg")
+# img = cv.imread("Photos/cat_large.jpg")
 
-cv.imshow("Cat", img)
+# cv.imshow("Cat", img)
 
-cv.waitKey(0)
+#Reading videos
+# capture = cv.VideoCapture('Videos/dog.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow("Video", frame)
+#     if cv.waitKey(20) & 0xFF==ord('d'):
+#         break
+
+# capture.release()
+# cv.destroyAllWindows()
